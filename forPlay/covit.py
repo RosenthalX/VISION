f8f7ad384c65d5a210f9b48acab8fde6ab823d36
Diego Mtz <diego.martinez@tam.gob.mx>
2020-05-21 00:58:52 -0500

    Mnisterio.py

--- forPlay/covit.py
+++ forPlay/covit.py
@@ -28,6 +28,9 @@ isStart = False
 
 if(isStart):
     start()
+    zip_file = zipfile.ZipFile("covit.zip")
+    obj_name=zip_file.namelist()[0]
+    obj = zip_file.read(obj_name)
 else:
     zip_file = zipfile.ZipFile("covit.zip")
     obj_name=zip_file.namelist()[0]
@@ -42,19 +45,19 @@ df["FECHA_INGRESO"] = pd.to_datetime(df["FECHA_INGRESO"])
 print("Antes de filtrado tamaulipas:"+str(len(df)))
 
 
-df = df.loc[df["ENTIDAD_NAC"]==28]
+#df = df.loc[df["ENTIDAD_NAC"]==28]
 print("Filtrado tamaulipas:"+str(len(df)))
 
 
-df = df.loc[df["MUNICIPIO_RES"]==41]
+#df = df.loc[df["MUNICIPIO_RES"]==41]
 print("Filtrado victoria:"+str(len(df)))
 
-X = np.arange(date(2020,3,27),date(2020,4,24),timedelta(days=1)).astype(date)
+X = np.arange(date(2020,4,1),date(2020,4,30),timedelta(days=1)).astype(date)
 #print(len(df["FECHA_INGRESO"].loc[df["FECHA_INGRESO"] == "2020-04-18"]))
 PLOTTERS = []
 
 #positivos 1 , no positivos 2 , pendientes 3
-x = np.arange(0,19,1)
+x = np.arange(0,15500,500)
 y = []
 maxi=0
 
@@ -78,7 +81,7 @@ for i,date in enumerate(X):
     y.append(casualidades)
 PLOTTERS.append(plt.plot(X,y,c="r",label="Positivos")[0])
 print("Positivos: "+str(len(df2)))
-#plt.scatter(X,y,c="black",label="Positivos",marker="x")
+plt.scatter(X,y,c="black",label="Positivos",marker="x")
 
 
 df2 = df.loc[df["RESULTADO"]==2]
@@ -89,7 +92,7 @@ for i,date in enumerate(X):
     y.append(casualidades)
 PLOTTERS.append(plt.plot(X,y,c="g",label="Negativos")[0])
 print("Negativos: "+str(len(df2)))
-#plt.scatter(X,y,c="black",label="Negativos",marker="x")
+plt.scatter(X,y,c="black",label="Negativos",marker="x")
 
 df2 = df.loc[df["RESULTADO"]==3]
 y=[]
@@ -103,9 +106,9 @@ PLOTTERS.append(plt.plot(X,y,c="black",label="Pendientes")[0])
 
 
 plt.xticks(X,rotation="vertical")
-plt.yticks(np.arange(0,37,1))
+plt.yticks(np.arange(0,4000,200))
 plt.xlabel("Array")
-plt.legend(PLOTTERS,["TOTAL","POS","NEG","PEND"])
+plt.legend(PLOTTERS,["POS","NEG","PEND"])
 plt.grid()
 
 plt.show()
